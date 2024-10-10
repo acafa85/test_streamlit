c92bcc4ac8b236756655803572d416ef1343292e
Cafa, Adrián <ACafa@pan-energy.com>
2024-10-10 11:21:47 -0300

    subiendo todo2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title('Hello, AWS App Runner with Streamlit and Docker!')
-st.write('This is a simple Streamlit app running on AWS App Runner using Docker.')
+st.title('Hello, AWS App Runner with Streamlit!')
+st.write('This is a simple Streamlit app running on AWS App Runner.')
